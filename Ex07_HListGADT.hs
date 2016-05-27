57a60c5c3d77b8c4c4808b71b1aa09009bc1b0e0
Andreas Herrmann <andreash87@gmx.ch>
2016-05-27 22:38:52 +0200

    Example 07

---
+++ Ex07_HListGADT.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Ex07_HListGADT where
+
+data HListG xs where
+    HNilG :: HListG '[]
+    HConsG :: x -> HListG xs -> HListG (x ': xs)
+
+-- type can be inferred
+listG :: HListG '[Char, Bool]
+listG = HConsG 'a' (HConsG True HNilG)
+
+-- type cannot be inferred
+-- won't compile without type-signature
+hLengthG :: HListG xs -> Int
+hLengthG HNilG = 0
+hLengthG (HConsG _ xs) = 1 + hLengthG xs
+
+-- type cannot be inferred
+-- won't compile without type-signature
+mysteriousG :: HListG '[Char, Bool] -> (String, Bool)
+mysteriousG = \(HConsG c (HConsG b HNilG)) -> (c:"", not b)
